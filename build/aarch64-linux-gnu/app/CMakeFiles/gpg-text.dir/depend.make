92ab7bbdb47f8315429be78c14fe22504cde4eb8
Tino Schurzmann <schurzmann@gmail.com>
2026-09-23 17:15:15 +0200

    Remove build artifacts from repository

--- build/aarch64-linux-gnu/app/CMakeFiles/gpg-text.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for gpg-text.
-# This may be replaced when dependencies are built.
--- build/aarch64-linux-gnu/app/CMakeFiles/gpg-text.dir/qrc_qml.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/gpg-text.dir/qrc_qml.cpp.o: \
- /home/tino/code/k/gpg-text/build/aarch64-linux-gnu/app/qrc_qml.cpp \
- /usr/aarch64-linux-gnu/include/stdc-predef.h
